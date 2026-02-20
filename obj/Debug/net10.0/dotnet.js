d7dc3e37849596ff2e31fe69f5e903378ae8055c
Michael Carrabine <mjc@irm.com>
2026-02-20 11:48:52 -0500

    Get Play Card to work with multiple players

--- obj/Debug/net10.0/dotnet.js
+++ obj/Debug/net10.0/dotnet.js
@@ -5,7 +5,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "MyFirstCastlePanic",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-yghv418Yjfnz1cwtCW4psJSbqmCmGMwaog3T/3RRGmo=",
+    "hash": "sha256-eTsyqNJjitP3den6GstfqBXA/G/r4ANFKeu+oOAiHzw=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.87vtjjdetb.js"
@@ -1242,16 +1242,16 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       },
       {
         "virtualPath": "MyFirstCastlePanic.wasm",
-        "name": "MyFirstCastlePanic.onlldpwogb.wasm",
-        "integrity": "sha256-HG7VwU/WGcOTmpf0ourHVHZPA1rWe/w8SIptU6SkUO0=",
+        "name": "MyFirstCastlePanic.4cz75is2t9.wasm",
+        "integrity": "sha256-pJtoz/6VdZ0pNDMnY5+OMFuaYPOFEHYQbD7XamePupg=",
         "cache": "force-cache"
       }
     ],
     "pdb": [
       {
         "virtualPath": "MyFirstCastlePanic.pdb",
-        "name": "MyFirstCastlePanic.oh2u7pdh8q.pdb",
-        "integrity": "sha256-5BxnHorFxhXkRznWZVNLYrryG5lCQFhqAnTy6KkY5fM=",
+        "name": "MyFirstCastlePanic.jiacnld84s.pdb",
+        "integrity": "sha256-O3s/Uue5J1wts28vCNRHtT1tMe1yEHM3XU+dj1VykAA=",
         "cache": "force-cache"
       }
     ],
